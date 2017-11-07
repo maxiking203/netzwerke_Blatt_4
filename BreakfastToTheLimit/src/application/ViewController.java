6dd82cb8105151fe880ca8da628f5a878d82a5ed
maxiking203 <Maximilian.Sachmann@gmx.de>
2017-11-07 22:22:23 +0100

    Teste request bei Button klick. Response wird in der Console angezeigt

--- BreakfastToTheLimit/src/application/ViewController.java
+++ BreakfastToTheLimit/src/application/ViewController.java
@@ -1,7 +1,13 @@
 package application;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import javax.net.ssl.HttpsURLConnection;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -17,11 +23,17 @@ import javafx.scene.control.Tooltip;
 
 public class ViewController implements Initializable {
 	
+	private static final String HOME = "Lothstr+34+80335";
+	private static final String TEST = "Lindacherstr+14+81249";
+	private final String USER_AGENT = "Mozilla/5.0";
+	
 	//Eingabe und Button Paula Pünktlich
 	@FXML
 	TextField pPStreet;
 	@FXML
-    TextField pPPLZ; 
+	TextField pPNr;
+	@FXML
+    TextField pPPlace; 
 	@FXML
 	ChoiceBox<String> pPKindOf; 
 	@FXML
@@ -31,9 +43,11 @@ public class ViewController implements Initializable {
     
 	//Eingabe und Button Lothar Late
 	@FXML
-    TextField lLStreet; 
+    TextField lLStreet;
 	@FXML
-	TextField lLPLZ;
+	TextField lLNr;
+	@FXML
+	TextField lLPlace;
 	@FXML
 	TextField lLTime;
 	@FXML
@@ -43,45 +57,104 @@ public class ViewController implements Initializable {
     
 	//Eingabe und Button Ich
 	@FXML
-    TextField iPLZ; 
+    TextField iPlace; 
 	@FXML
-	TextField iStreet; 
+	TextField iStreet;
+	@FXML
+	TextField iNr;
 	@FXML
 	TextField iTime;
 	@FXML
 	ChoiceBox<String> iKindOf; 
 	@FXML
 	Button iDone;
+
+	
+	@FXML
+	Button insert;
+	
 	@Override
-	
-	
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		// set choice box data for Paula Pünktlich
 		pPKindOf.setItems(FXCollections.observableArrayList("Auto", "zu Fuß", "Rad", "ÖPNV"));
-		pPKindOf.setTooltip(new Tooltip("Verkehrsmittel"));
 		pPKindOf.getSelectionModel().selectFirst();
 		
 		// set choice box data for Lothar Late
 		lLKindOf.setItems(FXCollections.observableArrayList("Auto", "zu Fuß", "Rad", "ÖPNV"));
-		lLKindOf.setTooltip(new Tooltip("Verkehrsmittel"));
 		lLKindOf.getSelectionModel().selectFirst();
 		
 		// set choice box data for Myself
 		iKindOf.setItems(FXCollections.observableArrayList("Auto", "zu Fuß", "Rad", "ÖPNV"));
-		iKindOf.setTooltip(new Tooltip("Verkehrsmittel"));
-		iKindOf.getSelectionModel().selectFirst();
+		iKindOf.getSelectionModel().selectFirst();;
 		
 	}
 	
+	//Paul Pünktlich left for work
 	public void pPEnd() {
 		
 	}
 	
+	//Lothar Late left for work
 	public void lLEnd() {
 		
 	}
+	
+	//I left for work
 	public void iEnd() {
 		
+	}
+	
+	//Insert finished, start request with input
+	public void toSend() {
+//		String pAdr = pPStreet.getText() + " " + pPPlace.getText();
+//		String pTime = pPTime.getText();
+//		String pKind = pPKindOf.getSelectionModel().getSelectedItem();
+//		
+//		String lAdr = lLStreet.getText() + " " + lLPlace.getText();
+//		String lTime = lLTime.getText();
+//		String lKind = lLKindOf.getSelectionModel().getSelectedItem();
+//		
+//		String iAdr  = iStreet.getText() + ", " + iPlace.getText();
+//		String iKind = iKindOf.getSelectionModel().getSelectedItem();
+//		String strITime  = iTime.getText();
+//		
+//		doRequest(pAdr, pTime, pKind);
+//		doRequest(lAdr, lTime, lKind);
+		doRequest();
+	}
+	
+	private void doRequest() {
+		String url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=" + HOME + "&destinations=" + TEST + "&key=AIzaSyD5FrhCIemscBuwJYQwoO6wLRuHceirDaY";
+			try {
+				HttpsURLConnection con = (HttpsURLConnection) new URL(url).openConnection();
+				con.setRequestMethod("GET");
+				//add request header
+				//con.setRequestProperty("User-Agent", USER_AGENT);
+
+				int responseCode = con.getResponseCode();
+				System.out.println("\nSending 'GET' request to URL : " + url);
+				System.out.println("Response Code : " + responseCode);
+
+				BufferedReader in = new BufferedReader(
+				        new InputStreamReader(con.getInputStream()));
+				String inputLine;
+				StringBuffer response = new StringBuffer();
+
+				while ((inputLine = in.readLine()) != null) {
+					response.append(inputLine);
+				}
+				in.close();
+				con.disconnect();
+
+				//print result
+				System.out.println(response.toString());
+				
+			} catch (IOException e) {
+
+				e.printStackTrace();
+			}
+
+
 	}
 	
 
